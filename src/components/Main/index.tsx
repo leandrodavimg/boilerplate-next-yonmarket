71ab375f3667650b29f74b16ef0d69c8413d68fa
Leandro Freitas <leandrofreitas@Leandros-MacBook-Pro.local>
2021-06-04 16:15:06 -0300

    insert tailwind in project

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,25 @@
 import * as S from './styles'
 
 const Main = () => (
-  <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo e React Avançado escrito ao lado."
-    />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>
-      TypeScript, ReactJS, NextJS e Styled Components
-    </S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para uma tela com código."
-    />
-  </S.Wrapper>
+  <>
+    <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado."
+      />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        TypeScript, ReactJS, NextJS e Styled Components
+      </S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela com código."
+      />
+    </S.Wrapper>
+  </>
 )
 
 export default Main
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
+import '../styles/tailwind.css'
 import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
